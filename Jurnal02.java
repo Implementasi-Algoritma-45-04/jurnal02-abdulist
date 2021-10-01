889d251c494eacdc2091b649a76195e33ba90448
Ahmad Abdul Fatah <dexel7zip@gmail.com>
2021-10-01 14:07:01 +0700

    Solusi jurnal-02

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+	public static void main (String[] args) {
+		System.out.println("NIM	: 6706213059");
+		System.out.println("Nama	: Ahmad Abdul Fatah");
+		System.out.println("Kelas	: D3IF-45-4");
+	}
+}
